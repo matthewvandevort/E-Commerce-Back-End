1ed31a0e6828369d59944a531c68b6fffcf1e604
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-29 12:43:23 -0400

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,61 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      ]
+    })
+      .then(productData => res.json(productData))
+      .catch(err =>{
+        console.log(err);
+        res.json(500).json(err);
+      });
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [
+        {
+          model: Category,
+          attributes: ['id', 'category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name']
+        }
+      ]
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No category exists with this id.' });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch(err =>{
+      console.log(err);
+      res.json(500).json(err);
+    });
 });
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -91,6 +139,23 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy ({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(productData => {
+      if (!productData) {
+        res.status(404).json({ message: 'No category exists with this id.' });
+        return;
+      }
+      res.json(productData);
+    })
+    .catch(err =>{
+      console.log(err);
+      res.json(500).json(err);
+    });
 });
+
 
 module.exports = router;
